14943ec5cf93b68554478c9f58e0e98cd33c7705
Thomas Collardeau <thomas.collardeau@clevertech.biz>
2017-04-14 19:54:03 +0200

    add firebase configs

---
+++ src/config.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase';
+
+const stagingConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
+  messagingSenderId: '',
+};
+
+const prodConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
+  messagingSenderId: '',
+};
+
+const isProd = process.env.NODE_ENV === 'production';
+const firebaseConfig = isProd ? prodConfig : stagingConfig;
+
+firebase.initializeApp(firebaseConfig);
+export const ref = firebase.database().ref();
+export const firebaseAuth = firebase.auth;
+export const storage = firebase.storage();
+export const messaging = firebase.messaging();
---
+++ sw/firebase-messaging-staging-sw.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+importScripts('https://www.gstatic.com/firebasejs/3.6.10/firebase-app.js');
+importScripts(
+  'https://www.gstatic.com/firebasejs/3.6.10/firebase-messaging.js'
+);
+
+const config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
+  messagingSenderId: '',
+};
+
+firebase.initializeApp(config);
+
+firebase.messaging();
